5b316c8d08b07b0fda2732a8c07eeefd1a8a493f
david02653 <david02653@gmail.com>
2020-10-22 01:29:59 +0800

    fix bugs

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,15 +1,15 @@
 public class Fibonacci {
 
     public static int getFibonacci(int n){
-        int n1 = 0, n2 = 1, temp = 0;
+        int n1 = 0, n2 = 1;
         if(n == 0) return 0;
         if(n == 1) return 1;
 
-        for(int i=0; i<n; i++){
-            temp = n1 + n2;
+        for(int i=1; i<n; i++){
+            int temp = n1 + n2;
             n1 = n2;
             n2 = temp;
         }
-        return n1 + n2;
+        return n2;
     }
 }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -15,6 +15,11 @@ public class FibonacciTest {
 
     @Test
     public void testCase3(){
-        assertEquals(5, Fibonacci.getFibonacci(3));
+        assertEquals(1, Fibonacci.getFibonacci(2));
+    }
+
+    @Test
+    public void testCase4(){
+        assertEquals(5, Fibonacci.getFibonacci(5));
     }
 }
